09bcbba5f9750532cf32632a88ec6a9efa08b233
Matthew Brown <mfb0308@gmail.com>
2021-01-15 19:38:01 -0500

    hmmm

--- gameoflife.c
+++ gameoflife.c
@@ -7,12 +7,16 @@ int main(int argc, char *argv[]){
         int sizeX = atoi(argv[1]);
         int sizeY = atoi(argv[2]);
 
+        printf("%d %d\n", sizeX, sizeY);
+
 
         char board[sizeX][sizeY];
 
         for (int i = 0; i < sizeX; i++){
                 for (int j = 0; j < sizeY; j++){
-                        if (i == 0 || j == 0 || (j == sizeY - 1) || (i == sizeX - 1)){
+                        if (i == 0 || j == 0){
+                                printf("# ");
+                        } else if (i == sizeX - 1 || j == sizeY - 1){
                                 printf("# ");
                         } else {
                                 printf(" ");
