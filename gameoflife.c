a17e7a3d28c848eeafe6dd19ccc015b5407efd3c
Matthew Brown <mfb0308@gmail.com>
2021-01-15 19:19:42 -0500

    made a board

--- gameoflife.c
+++ gameoflife.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char argv[]){
+int main(int argc, char *argv[]){
 
-        
+        int size = atoi(argv[1]);
+
+        char board[size][size];
+
+        for (int i = 0; i < size; i++){
+                for (int j = 0; j < size; j++){
+                        printf("1");
+                }
+                printf("\n");
+        }
 
 }
